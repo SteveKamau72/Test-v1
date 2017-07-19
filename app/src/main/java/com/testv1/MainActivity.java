98daf527a3a7e6c7e5b2e8e68d7f84627a8222b2
Steve Kamau <stevekamau72@gmail.com>
2017-07-19 17:12:46 +0300

    Referenced views from layout

--- app/src/main/java/com/testv1/MainActivity.java
+++ app/src/main/java/com/testv1/MainActivity.java
@@ -2,12 +2,25 @@ package com.testv1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button btnGo;
+    TextView tvResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setViews();
+    }
+
+    private void setViews() {
+        tvResults = (TextView) findViewById(R.id.result);
+        editText = (EditText) findViewById(R.id.editText);
+        btnGo = (Button) findViewById(R.id.go);
     }
 }
