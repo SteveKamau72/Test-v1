ebafa35aa077cbced7e81184489a8495a01f010a
Steve Kamau <stevekamau72@gmail.com>
2017-07-19 19:06:52 +0300

    Added test for empty input

--- app/src/androidTest/java/com/testv1/MainActivityTest.java
+++ app/src/androidTest/java/com/testv1/MainActivityTest.java
@@ -1,10 +1,16 @@
 package com.testv1;
 
+import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * Created by steve on 7/19/17.
@@ -16,6 +22,14 @@ public class MainActivityTest {
     @Before
     public void setUp() throws Exception {
         System.out.println("Start unit test!");
+    }
+
+    @Test
+    public void testEmptyInputScenario() {//user clicks button with empty input
+        //input some text
+        Espresso.onView(withId(R.id.editText)).perform(typeText(""));
+        //perform button click
+        Espresso.onView(withId(R.id.go)).perform(click());
     }
 
     @After
