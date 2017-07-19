ba4ac91e4b5b5370c9f2b751799efe08118eef35
Steve Kamau <stevekamau72@gmail.com>
2017-07-19 19:17:47 +0300

    Added test for valid input and summation

--- app/src/androidTest/java/com/testv1/MainActivityTest.java
+++ app/src/androidTest/java/com/testv1/MainActivityTest.java
@@ -10,7 +10,9 @@ import org.junit.Test;
 
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by steve on 7/19/17.
@@ -19,6 +21,7 @@ public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
     private String invalidTestValues = "ffe,123";
+    private String validTestValues = "125, 254, 10, 6, 4765";
 
     @Before
     public void setUp() throws Exception {
@@ -39,6 +42,16 @@ public class MainActivityTest {
         Espresso.onView(withId(R.id.editText)).perform(typeText(invalidTestValues));
         //perform button click
         Espresso.onView(withId(R.id.go)).perform(click());
+    }
+
+    @Test
+    public void testUserInputScenario() {//user clicks button with valid input
+        //input some text
+        Espresso.onView(withId(R.id.editText)).perform(typeText(validTestValues));
+        //perform button click
+        Espresso.onView(withId(R.id.go)).perform(click());
+        //checking the result in textview
+        Espresso.onView(withId(R.id.result)).check(matches(withText("48")));
     }
 
     @After
